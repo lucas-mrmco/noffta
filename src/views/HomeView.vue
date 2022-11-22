39db3e6584e03ef62dbd33f11628b68d23daff0f
lucas-mrmco <lucas.moramarco@edu.univ-fcomte.fr>
2022-11-22 17:04:30 +0100

    rattrapage error git

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,18 +5,25 @@ import RocketVue from '../components/Rocket.vue';
 <template>
   <main>
     <h2 class="font-nunito-sans text-xl text-blanc-text">Noffta supports you in the creation of NFT</h2>
-    <p class="text-blanc-text font-lato text-sm">Indeed, you will be able to design, modify, sign, publish and even print your own NFT.</p>
-
+    <p class="text-blanc-text font-lato text-sm">Indeed, you will be able to design, modify, sign, publish and even
+      print your own NFT.</p>
+    <img class="object-center" src="../assets/illustration_NFT.png" alt="illustration réseau NFT" />
     <button class="rounded-7xl h-11 w-32 bg-gradient-to-r from-mauve to-orange inline-flex items-center p-6">
-      <RocketVue/>
+      <RocketVue />
       <span class="text-blanc-text font-lato text-sm">Create</span>
     </button>
-    
+    <button class="rounded-7xl h-11 w-32 border-gradient-to-r from-mauve to-orange inline-flex items-center p-6">
+      <RocketVue />
+      <span class="text-blanc-text font-lato text-sm">Explore all</span>
+    </button>
+    <h3 class="font-nunito-sans text-lg text-blanc-text">What is NOFFTA ?</h3>
+
   </main>
 </template>
 
 <style scoped>
-h2 {
-  text-shadow:  -1px 1.5px 0px #F81DFB ;
+h2,
+h3 {
+  text-shadow: -1px 1.5px 0px #F81DFB;
 }
 </style>
