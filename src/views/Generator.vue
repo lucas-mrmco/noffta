f45d64a1fb32588532a54d2982287cf9c1840083
lucas-mrmco <lucas.moramarco@edu.univ-fcomte.fr>
2022-12-05 17:11:05 +0100

    generator v2

--- src/views/Generator.vue
+++ src/views/Generator.vue
@@ -1,19 +1,36 @@
 <template>
-  <h2 class="font-nunito-sans text-lg text-blanc-text pb-6 pt-10 text-center">  Create your own NFT in just a few seconds !</h2>
+  <h2 class="font-nunito-sans text-lg text-blanc-text pb-6 pt-10 text-center"> Create your own NFT in just a few seconds
+    !</h2>
   <p class="text-blanc-text font-lato text-sm bold text-center">Free of charge !</p>
   <div class="grid">
-  <button> Create NFT </button>
-  <img class="content-center w-72 pt-20" src="../assets/illustration_generator.png" alt="illustration générateur de NFT" />
+    <button> Create NFT </button>
+    <img class="content-center w-72 pt-20" src="../assets/illustration_generator.png"
+      alt="illustration générateur de NFT" />
   </div>
   <h3 class="text-blanc-text font-lato text-sm bold pt-40">Discover all</h3>
   <p class="text-red-600">A FAIRE Cards ...</p>
   <h2 class="font-nunito-sans text-lg text-blanc-text pb-6 pt-10 text-center">NFT in progress</h2>
   <p class="text-blanc-text font-nunito-sans text-sm text-center">Step 1/10</p>
+  <div class="rounded-2xl w-80 h-60 mx-auto  mt-10 bg-gradient-to-r p-1 from-mauve to-orange">
+    <div class="flex flex-col justify-between h-full bg-fond-site text-white rounded-lg p-4">
+      <p class="text-blanc-text font-lato text-sm bold text-center">Have you ever attended the OFFF ?</p>
+      <div class="flex items-center">
+        <input id="default-radio-1" type="radio" value="" name="default-radio"
+          class=" w-4 h-4 bg-gradient-to from-rose to-mauve border-rose focus:ring-rose">
+        <label for="default-radio-1" class="ml-2 font-lato text-sm bold text-blanc-text">Yes</label>
+      </div>
+      <div class="flex items-center">
+        <input checked id="default-radio-2" type="radio" value="" name="default-radio"
+          class="w-4 h-4 bg-gradient-to from-rose to-mauve border-rose ">
+        <label for="default-radio-2" class="ml-2 font-lato text-sm bold text-blanc-text">No</label>
+      </div>
+    </div>
+  </div>
 
-  <div class="rounded-2xl w-[340px] h-[233px] mx-auto  mt-10 bg-gradient-to-r p-[6px] from-rose-400 to-orange-400">
-   <div class="flex flex-col justify-between h-full bg-black text-white rounded-lg p-4">
-     </div>
-</div>
+
+
+
+
 </template>
 
 
@@ -23,3 +40,4 @@ h3 {
   text-shadow: -1px 1.5px 0px #F81DFB;
 }
 </style>
+
