7372f2d34029788e3621f87681480b950acdd625
lucas-mrmco <lucas.moramarco@edu.univ-fcomte.fr>
2023-01-16 23:56:02 +0100

    meta + connect supabase + insccription // quelques problèmes de connexion "signin" is not a function

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,4 +1,6 @@
-<script>
+<script setup>
+import { createClient } from '@supabase/supabase-js'
+import { SupabaseAuthClient } from '@supabase/supabase-js/dist/module/lib/SupabaseAuthClient'
 </script>
 
 <template>
@@ -18,7 +20,7 @@
                         </label>
                         <input
                             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="username" type="text">
+                            id="username" type="text" v-model="email">
                     </div>
                     <div class="mb-6 lg:px-14 ">
                         <label class="block text-white text-sm mb-2" for="password">
@@ -26,7 +28,7 @@
                         </label>
                         <input
                             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                            id="password" type="password">
+                            id="password" type="password" v-model="passwd">
                     </div>
                     <div class="text-center">
                         <a class="inline-block align-baseline text-white  text-sm mb-9 " href="#">
@@ -43,12 +45,12 @@
                             </RouterLink>
                         </div>
                         <div>
-                            <RouterLink to="/profil" class="flex-row justify-end">
-                                <button type="button"
-                                    class="lg:w-36 w-24 text-base rounded-3xl border-2 bg-mauve p-3 text-center text-white font-light ">
-                                    SIGN IN
-                                </button>
-                            </RouterLink>
+                            <!-- <RouterLink to="/profil" class="flex-row justify-end"> -->
+                            <button @click="login2()" type="button"
+                                class="lg:w-36 w-24 text-base rounded-3xl border-2 bg-mauve p-3 text-center text-white font-light ">
+                                SIGN IN
+                            </button>
+                            <!-- </RouterLink> -->
                         </div>
                     </div>
                 </form>
@@ -71,4 +73,82 @@ h3 {
     text-shadow: -1px 1.5px 0px #F81DFB;
 }
 </style>
+
+<script>
+const SUPABASE_URL = 'https://wwokdzydrgxbfxtektwz.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3b2tkenlkcmd4YmZ4dGVrdHd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk3OTcwNzYsImV4cCI6MTk4NTM3MzA3Nn0.5gn7vAfpwNQp5yFyZHLLxIvIAm5bhCQaUGE4CKH2whI'
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+supabase.auth.onAuthStateChange((event, session) => {
+    if (session == null) {
+        document.getElementById('status').innerHTML = 'You are not logged !!!';
+    } else {
+        //alert('session value: ' + JSON.stringify(session)) 
+        document.getElementById('status').innerHTML = 'You are logged with the email: ' + session.user.email;
+    }
+})
+export default {
+    methods: {
+        //this method allows to release the connexion with the Google account 
+        async logout() {
+            try {
+                const { user, session, error } = await supabase.auth.signOut();
+                if (error) throw error;
+                document.getElementById('status').innerHTML = 'You are disconnected !'
+            } catch (error) {
+                alert(error.error_description || error.message);
+            }
+        },
+        //this method allows to log in the system using Google provider 
+
+        async loginGoogle() {
+            try {
+                const { user, session, error } = await supabase.auth.signIn({
+                    provider: 'google',
+                });
+                if (error) throw error;
+            } catch (error) {
+                alert(error.error_description || error.message);
+            }
+        },
+        async loginGithub() {
+            try {
+                const { user, session, error } = await supabase.auth.signIn({
+                    provider: 'github',
+                });
+                if (error) throw error;
+            } catch (error) {
+                alert(error.error_description || error.message);
+            }
+        },
+        async login() {
+            try {
+                const { user, session, error } = await supabase.auth.signIn({
+                    email: this.email,
+                    password: this.passwd
+                })
+                if (error) throw error;
+                else {
+                    document.getElementById('signOut').style.visibility = 'hidden'
+                    document.getElementById('addPoem').style.visibility = 'visible'
+                }
+            } catch (error) {
+                alert(error.error_description || error.message);
+            }
+
+        },
+        async login2() {
+            try {
+                const { user, session, error } = await supabase.auth.signIn({
+                    email: this.email,
+                    password: this.passwd
+                })
+                if (error) throw error;
+            } catch (error) {
+                alert(error.error_description || error.message);
+            }
+        }
+    }
+}
+
+</script>
 
--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,6 +1,7 @@
-<script>
+<script setup>
+import { createClient } from '@supabase/supabase-js'
+import { SupabaseAuthClient } from '@supabase/supabase-js/dist/module/lib/SupabaseAuthClient'
 </script>
-
 <template>
 
     <main class="lg:pt-10 pt-28 px-10 lg:px-28">
@@ -39,7 +40,7 @@
                         </label>
                         <input
                             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="username" type="text">
+                            id="username" type="text" required v-model="email">
                     </div>
                     <div class="mb-4 lg:px-14">
                         <label class="block text-white text-sm mb-2" for="password">
@@ -47,9 +48,9 @@
                         </label>
                         <input
                             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                            id="password" type="password">
+                            id="password" type="password" required v-model="passwd">
                     </div>
-                    <div class="mb-4 lg:px-14">
+                    <div class=" mb-4 lg:px-14">
                         <label class="block text-white text-sm mb-2" for="password">
                             CONFIRM PASSWORD
                         </label>
@@ -63,7 +64,8 @@
                             <input id="default-checkbox" type="checkbox" value=""
                                 class="w-4 h-4 bg-gray-100 rounded border-gray-300 ">
                             <label for="default-checkbox"
-                                class="ml-2 text-sm font-medium text-white  dark:text-gray-300">I accept the Conditions
+                                class="ml-2 text-sm font-medium text-white  dark:text-gray-300">I
+                                accept the Conditions
                                 Generals of Utilisation</label>
                         </div>
                     </div>
@@ -77,12 +79,12 @@
                             </a>
                         </div>
                         <div>
-                            <a href="/login" class="flex-row justify-end">
-                                <button type="button"
-                                    class="lg:w-36 w-24 text-base rounded-3xl border-2 bg-mauve p-3 text-center text-white font-light ">
-                                    SIGN UP
-                                </button>
-                            </a>
+                            <!-- <a href="/login" class="flex-row justify-end"> -->
+                            <button v-on:click="register()" type="button"
+                                class="lg:w-36 w-24 text-base rounded-3xl border-2 bg-mauve p-3 text-center text-white font-light ">
+                                SIGN UP
+                            </button>
+                            <!-- </a> -->
                         </div>
                     </div>
                 </form>
@@ -105,3 +107,66 @@ h3 {
     text-shadow: -1px 1.5px 0px #F81DFB;
 }
 </style>
+
+
+<script>
+const SUPABASE_URL = 'https://wwokdzydrgxbfxtektwz.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3b2tkenlkcmd4YmZ4dGVrdHd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk3OTcwNzYsImV4cCI6MTk4NTM3MzA3Nn0.5gn7vAfpwNQp5yFyZHLLxIvIAm5bhCQaUGE4CKH2whI'
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+supabase.auth.onAuthStateChange((event, session) => {
+    if (session == null) {
+        document.getElementById('status').innerHTML = 'You are not logged !!!';
+    } else {
+        //alert('session value: ' + JSON.stringify(session)) 
+        document.getElementById('status').innerHTML = 'You are logged with the email: ' + session.user.email;
+    }
+})
+export default {
+    methods: {
+        //this method allows to release the connexion with the Google account 
+        async logout() {
+            try {
+                const { user, session, error } = await supabase.auth.signOut();
+                if (error) throw error;
+                document.getElementById('status').innerHTML = 'You are disconnected !'
+            } catch (error) {
+                alert(error.error_description || error.message);
+            }
+        },
+        //this method allows to log in the system using Google provider 
+
+        async loginGoogle() {
+            try {
+                const { user, session, error } = await supabase.auth.signIn({
+                    provider: 'google',
+                });
+                if (error) throw error;
+            } catch (error) {
+                alert(error.error_description || error.message);
+            }
+        },
+        async loginGithub() {
+            try {
+                const { user, session, error } = await supabase.auth.signIn({
+                    provider: 'github',
+                });
+                if (error) throw error;
+            } catch (error) {
+                alert(error.error_description || error.message);
+            }
+        },
+        async register() {
+            try {
+                const { user, session, error } = await supabase.auth.signUp({
+                    email: this.email,
+                    password: this.passwd
+                })
+                if (error) throw error;
+            } catch (error) {
+                alert(error.error_description || error.message);
+            }
+        }
+    }
+}
+
+</script>
